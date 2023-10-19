17aea4d2927ccb72bbf3062f88ae2e27de71def0
kevy60 <kevin.vorsmann@voco.ee>
2023-10-19 22:04:55 +0300

    8.Sündmuste töötlus komponentides

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,31 @@
-import './ExpenseItem.css'
+import React, { useState } from 'react';
+import './ExpenseItem.css';
 import ExpenseDate from "./ExpenseDate.js";
+import Card from "../UI/Card";
 
-function ExpenseItem(props){
+const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.expenseData.title);
+    const [isUpdated, setIsUpdated] = useState(false);
+
+    const clickHandler = () => {
+        if (isUpdated) {
+            setTitle(props.expenseData.title); 
+        } else {
+            setTitle('Updated!'); 
+        }
+        setIsUpdated(!isUpdated); 
+    }
 
     return (
         <div className="expense-item">
             <ExpenseDate date={props.expenseData.date}></ExpenseDate>
             <div className="expense-item__description">
-                <h2>{props.expenseData.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{props.expenseData.price}€</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </div>
-    )
+    );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
